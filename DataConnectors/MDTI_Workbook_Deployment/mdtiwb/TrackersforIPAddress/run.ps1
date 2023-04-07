59373b9ccf3c50108ade31b6b78cc71cee532072
Yaniv Shasha <yanivsh@microsoft.com>
2023-04-07 08:15:10 -0700

    co

---
+++ DataConnectors/MDTI_Workbook_Deployment/mdtiwb/TrackersforIPAddress/run.ps1
@@ -0,0 +1,43 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$clientId = $env:clientId
+$tenantId = $env:tenantId
+$clientSecret = $env:clientSecret
+
+
+# Construct URI
+$uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+
+# Construct Body
+$body = @{
+    client_id     = $clientId
+    scope         = "https://graph.microsoft.com/.default"
+    client_secret = $clientSecret
+    grant_type    = "client_credentials"
+}
+
+# Get OAuth 2.0 Token
+$tokenRequest = Invoke-WebRequest -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing
+
+# Access Token
+$token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+
+#$headers = $Request.Headers
+$ipaddress = $Request.query["ipaddress"]
+
+
+# graphAPI Get users properties
+$apiUrl = "https://graph.microsoft.com/beta/security/threatIntelligence/hosts($ipaddress)/trackers?$count=true"
+$apiUrl
+$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $Token"} -Uri $apiUrl -Method Get -ContentType aplication/json -UseBasicParsing
+$obj = $Data | convertTo-Json
+
+
+# Return Write-OMSLogfile response to output binding
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    Body = $obj
+
+})
